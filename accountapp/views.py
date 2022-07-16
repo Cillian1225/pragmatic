73ee1696ea6a15209691f9ecc976a1f06e94a76c
Cillian Jung93 <86462224+Cillian1225@users.noreply.github.com>
2022-07-16 23:38:12 +0900

    django course 17 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from accountapp.models import Helloworld
+
 
 def hello_world(request):
-    if request.method =="POST":
-        return render(request, 'accountapp/hello_world.html',context={'text' : 'POST METHOD!!'})
+    if request.method == "POST":
+
+        temp = request.POST.get("hello_world_input")
+
+        new_hello_world = Helloworld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!'})
 
