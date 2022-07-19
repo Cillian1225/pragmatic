e60c3b4ad4fa34581d520419028d51677401d077
Cillian Jung93 <86462224+Cillian1225@users.noreply.github.com>
2022-07-19 23:51:42 +0900

    django course 31  profileCreateView form_valid

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,12 +5,12 @@ from django.db import models
 from pyexpat import model
 
 
-class Profile(models.Model) :
-    user = model.OntoOneField(User, on_delete=models.CASCADE, related_name = 'profile')
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     #이미지 저장경로
-    image = model.ImageField(upload_to='profile/', null=True)
-    nickname = models.CharField(max_length=20, unique=True, null = True)
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
 
 
