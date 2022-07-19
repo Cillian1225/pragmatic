79a1819d6f4c983a4fef03854fd659f39774e8ca
Cillian Jung93 <86462224+Cillian1225@users.noreply.github.com>
2022-07-20 00:33:48 +0900

    django corse 33 profilechang menu

--- profileapp/models.py
+++ profileapp/models.py
@@ -8,7 +8,6 @@ from pyexpat import model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
-    #이미지 저장경로
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     message = models.CharField(max_length=100, null=True)
