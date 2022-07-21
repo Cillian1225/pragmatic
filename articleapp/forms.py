666a55bc813fe9e4e93cfdb8ea593ecdfc862b00
Cillian Jung93 <86462224+Cillian1225@users.noreply.github.com>
2022-07-22 01:29:49 +0900

    django course 35 Articleapp CRUD

---
+++ articleapp/forms.py
@@ -0,0 +1,12 @@
+from _curses import meta
+
+from django.forms import ModelForm
+
+from articleapp.models import Article
+
+
+class ArticleCreationForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'image', 'content']
+
